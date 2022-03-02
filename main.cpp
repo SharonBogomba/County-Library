11e83d3931afdeec7dd04240f2fa34ef43ddae48
SharonBogomba <98330781+SharonBogomba@users.noreply.github.com>
2022-03-02 22:24:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+A County Library Management System
+Created by Sharon Bogomba
+on Feb, 2022
+C89 Compiler
+MIT License
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   printf("County library\n");
+    printf("Welcome Sharon. \n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("1. Add Book.\n");
+    printf("Selected action: ");
+    return 0;
+}
